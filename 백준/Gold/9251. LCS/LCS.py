8e84488f2f44e4e1630315a1c739d94551d96252
Soobin Kim <81364415+Soobin-329@users.noreply.github.com>
2024-11-28 21:57:03 +0900

    [Gold V] Title: LCS, Time: 492 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -4,14 +4,16 @@
 s1 = input().strip()
 s2 = input().strip()
 
-dp = [[0] * (len(s2)+1) for _ in range(len(s1) + 1)]
+dp = [[0] * (len(s1)+1) for _ in range(len(s2)+1)] # 열 행
 
-for a in range(1, len(s1) + 1):
-    for b in range(1, len(s2) + 1):
-        if s1[a - 1] == s2[b - 1]:
-            dp[a][b] = dp[a-1][b-1] + 1
+# 첫 행, 열 세팅 -> 없어도됨???? ㅇㅇㅇ 제일 처음 '' 공백은 같은게 없는거라고 치고 0이라고 하면 맞지 ㅇㅇ
 
+for r in range(1, len(s2)+1):
+    for c in range(1, len(s1)+1):
+        if s1[c-1] == s2[r-1]:
+            dp[r][c] = dp[r-1][c-1] + 1
         else:
-            dp[a][b] = max(dp[a-1][b], dp[a][b-1])
+            dp[r][c] = max(dp[r][c-1], dp[r-1][c])
 
-print(dp[-1][-1])
+
+print(dp[len(s2)][len(s1)])

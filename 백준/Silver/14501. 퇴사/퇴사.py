132b103d8ccce8fe87ea5fe218f2691aeefa8006
Soobin Kim <81364415+Soobin-329@users.noreply.github.com>
2025-02-12 15:28:39 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,37 +1,28 @@
 # 퇴사
 # 실버 3
-# 재귀적 풀이
+# DP - Bottom up 방식
 
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-plan = [(0,0)]
+plan = []
 
-for _ in range(n):
-    t, p = map(int, input().split())
-    plan.append((t,p))
+for i in range(n):
+    plan.append(list(map(int, input().split())))
 
+# DP 사용
+# 각 날짜의 최대 수익을 담는 것
+dp = [0 for i in range(n + 1)]
 
-def work(day, money):
-    global answer
-    if day > n:
-        answer = max(answer, money)
-        return
+for day in range(n):
+    # (day + 상담기간) 부터 퇴사일까지 반복문
+    for next in range(day + plan[day][0], n + 1):
+        # 상담으로 얻을 수 있는 수익이 이때까지 dp에서 갖고있던 수익보다 더 크면 갱신
+        # 예를 들어, 1일차에 3일걸리는 10가치의 상담을 진행했을 때,
+        # dp는 이렇게 순서대로 바뀔 것임
+        # [0,0,0,10,0,0,0,0], [0,0,0,10,10,0,0,0], ... , [0,0,0,10,10,10,10,10]
+        if dp[next] < dp[day] + plan[day][1]:
+            dp[next] = dp[day] + plan[day][1]
 
-    time, value = plan[day]
-
-    if day + time <= (n + 1):
-        # 그 날의 일을 받는 것과 안 받는 것 둘 다
-        work(day + time, money + value)
-        work(day + 1, money)
-    else:
-        # 퇴사일 이후의 일이라면 그 다음날로 이동
-        work(day + 1, money)
-
-
-answer = 0
-for i in range(1, n + 1):
-    work(i, 0)
-
-print(answer)
+print(dp[-1])
